ac25f04486f4aa047cd8680526f65b6915b131ff
Marcel <146003678+marwoz01@users.noreply.github.com>
2025-11-20 09:33:28 +0100

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,14 +3,13 @@ import { sedgwickAve } from "../fonts";
 export default function AboutMePage() {
   return (
     <p className="fixed bottom-8 right-8 p-6 text-zinc-300 text-right w-100">
-      I’m a passionate Frontend Developer focused on building{" "}
+      I’m a passionate Web Developer focused on building{" "}
       <span className={sedgwickAve.className}>clean</span>,{" "}
       <span className={sedgwickAve.className}>responsive</span> interfaces and
       smooth user experiences. I enjoy combining technical precision with{" "}
       <span className={sedgwickAve.className}>creativity</span>, turning ideas
       into functional, visually appealing web projects. Currently, I’m
-      developing my skills in React and exploring modern tools like Tailwind CSS
-      and Next.js to create fast and dynamic websites.
+      developing my skills in React and exploring modern tools like Tailwind CSS, Next.js and Express.jsto create fast and dynamic websites.
     </p>
   );
 }
